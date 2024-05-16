4aa7e6963f504ed0e21957aa50d5c1c90c6c5bae
gabrielle978 <gbassiquete@outlook.com>
2024-05-15 23:45:53 -0300

    Alterações feita em repositório 'trilha-net-poo-desafio'

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,13 +3,23 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string IMEI{ get; set;}
 
-        public Smartphone(string numero)
+        private string Modelo {get; set;}
+
+        private int Memoria {get; set; }
+
+
+        public Smartphone(string numero, string imei, string modelo, int memoria)
+        
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            IMEI = imei;
+            Modelo = modelo;
+            Memoria = memoria;
         }
+
+
 
         public void Ligar()
         {
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+    Console.WriteLine("Informações smartphone: Nokia");
+    Smartphone nokia = new Nokia("2024", "123456789", "xz-1002", 8);
+    nokia.InstalarAplicativo("Obsidian");
+    nokia.Ligar();
+    
+    Console.WriteLine("\n");
+
+    Console.WriteLine("Informações smartphone: Iphone");
+    Smartphone iphone = new Iphone("2025", "987654321", "apple098", 4);
+    iphone.InstalarAplicativo("Github");
+    iphone.ReceberLigacao();
